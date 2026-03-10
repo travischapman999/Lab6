86c0b21721a81a0cd7f6c78446676a20bb62514b
travis.chapman <travis.chapman@ENGRCKCP5S3>
2026-03-10 13:42:46 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.Write("Please input a number: ");
+        int Input = int.Parse(Console.ReadLine());
+        int Square;
+        int Cube;
+        GetSquareAndCube(Input, out Square, out Cube);
+        void GetSquareAndCube(int input, out int squared, out int cubed)
+        {
+            squared = input*input;
+            cubed = squared*input;
+        }
+        Console.WriteLine($"Squared = {Square}");
+        Console.WriteLine($"Cubed = {Cube}");
+    }
+}
