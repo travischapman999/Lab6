75d2263fc3cdafe916ab1d6c74004c66e6aa100c
travis.chapman <travis.chapman@ENGRCKCP5S3>
2026-03-10 14:21:10 -0600

    Working with DateTime

--- Program.cs
+++ Program.cs
@@ -3,15 +3,12 @@ public class Program
 {
     static void Main()
     {
-        Console.Write("Please input a number: ");
-        int Input = int.Parse(Console.ReadLine());
-        if (Input.IsEven())
-        {
-            Console.WriteLine("The input was even");
-        }
-        else
-        {
-            Console.WriteLine("The input was odd");
-        }
+        Console.Write("Enter a date(yyyy/mm/dd): ");
+        string Input = Console.ReadLine();
+        DateTime InputDate = DateTime.Parse(Input);
+        DateTime TodaysDate = DateTime.Now;
+        Console.WriteLine($"You entered: {InputDate:dddd}, {InputDate:MMMM} {InputDate.Day}, {InputDate:yyyy}");
+        TimeSpan TimeSinceInput = TodaysDate.Subtract(InputDate);
+        Console.WriteLine($"Days since date entered: {TimeSinceInput.Days}");
     }
 }
